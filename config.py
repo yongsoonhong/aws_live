6cec8b2484f1ff1a41332914c289e9eec7a03fee
yongsoonhong <94907094+yongsoonhong@users.noreply.github.com>
2023-09-10 15:35:17 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "employee.cspnc5ouklrd.us-east-1.rds.amazonaws.com"
+customhost = "employee.caitdxgajow9.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee"
+custombucket = "yongsoonhong-employee"
 customregion = "us-east-1"
 
